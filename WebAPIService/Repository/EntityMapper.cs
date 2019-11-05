ec9b7e69dde4fa0efbbc555044d52de8a2af3926
vicse <vicse.ore@tecsup.edu.pe>
2019-11-04 22:36:31 -0500

    First commit

---
+++ WebAPIService/Repository/EntityMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataAccessLayer;
+using AutoMapper;
+
+namespace WebAPIService.Repository
+{
+    public class EntityMapper<TSource, TDestination> where TSource: class where TDestination : class
+    {
+
+        public EntityMapper()
+        {
+            Mapper.CreateMap<Models.Product, Product>();
+            Mapper.CreateMap<Product, Models.Product>();
+
+        }
+        
+        public TDestination Translate(TSource obj)
+        {
+            return Mapper.Map<TDestination>(obj);
+        }
+
+    }
+}
